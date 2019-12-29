b9c8f1d044355ffbb5ae26219a9740991e77a367
EarthSurawit <surawitwiriya@kkumail.com>
2019-12-29 22:46:19 +0700

    Updates

---
+++ precache-manifest.300ceaa2403f6d37f97ac1447ce7a627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379b01e373e7c91fb93879f6147f6cd0",
+    "url": "/surawit-w/smartfarm-website.git/index.html"
+  },
+  {
+    "revision": "7e89e1aed26c043c7f03",
+    "url": "/surawit-w/smartfarm-website.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb00de41366120efa94f",
+    "url": "/surawit-w/smartfarm-website.git/static/js/2.78313752.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/surawit-w/smartfarm-website.git/static/js/2.78313752.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e89e1aed26c043c7f03",
+    "url": "/surawit-w/smartfarm-website.git/static/js/main.abf98211.chunk.js"
+  },
+  {
+    "revision": "7a03daba5d5063239729",
+    "url": "/surawit-w/smartfarm-website.git/static/js/runtime-main.280d9045.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/surawit-w/smartfarm-website.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
