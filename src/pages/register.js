94e78a04556c47461c07892a0f1e942f0f8e3470
Taewapon.B <petchz2012@hotmail.com>
2020-02-17 21:41:12 +0700

    Version 0.4.5
    Added loading screen with good ux

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import "../App.css";
 import axios from "axios";
-import { Button, Header, Form, Icon, Transition } from "semantic-ui-react";
+import {
+  Button,
+  Header,
+  Form,
+  Icon,
+  Transition,
+  Loader,
+  Dimmer
+} from "semantic-ui-react";
 
 const liff = window.liff;
 const API = "https://line-smartfarm-api.herokuapp.com";
@@ -16,14 +24,19 @@ export default class register extends Component {
       name: "",
       tel: "",
       loading: false,
-      visible: false
+      loader: false,
+      verify: false
     };
     this.initialize = this.initialize.bind(this);
   }
 
   componentDidMount() {
-    // this.initialize();
     document.title = "Register";
+    setTimeout(() => {
+      this.setState ({ loader: true });
+      this.initialize();
+      // this.verify(); //use this function if test
+    }, 1000);
   }
 
   initialize() {
@@ -49,7 +62,6 @@ export default class register extends Component {
   // check for duplicated UID
   verify() {
     console.log("Checking for duplicated...");
-    this.setState({ loading: true });
     axios
       .post(API + "/verify", {
         uid: this.state.uid
@@ -57,13 +69,17 @@ export default class register extends Component {
       .then(res => {
         // console.log(res);
         console.log("New UID Detected!");
-        this.setState({ visible: true,loading: false })
-      }).catch(err => {
+        this.setState({ loader: false});
+        setTimeout(() => {
+          this.setState({ verify: true });
+        }, 1500);
+      })
+      .catch(err => {
         // console.log(err);
         console.log("Duplicated UID Detected!");
         liff.closeWindow();
-      })
-  };
+      });
+  }
 
   // post a form data to DB
   register = e => {
@@ -100,62 +116,88 @@ export default class register extends Component {
   render() {
     return (
       <div>
-        <div id="loader" className="main">
-          
-        </div>
-        <div id="register-form" className="main">
-          <Transition
-            visible={this.state.visible}
-            animation="scale"
-            duration={1000}
-          >
-            <Form onSubmit={this.register}>
-              <Header as="h1" icon className="prompt">
-                <Icon name="pencil alternate" />
-                ลงทะเบียนผู้ใช้ใหม่
-                <Header.Subheader className="subheader">
-                  {this.state.loading
-                    ? "กรุณารอสักครู่ กำลังประมวลผลค่ะ" : "โปรดกรอกข้อมูลก่อนเริ่มใช้งานด้วยค่ะ"}
-                </Header.Subheader>
-              </Header>
-              <Form.Field required>
-                <label className="form-label">ชื่อเกษตรกร</label>
-                <input
-                  required
-                  className="form-input"
-                  placeholder="กรอกชื่อที่นี่"
-                  type="text"
-                  name="name"
-                  value={this.state.name}
+        {this.state.verify ? (
+          // register form
+          <div id="register-form" className="main">
+            <Transition
+              visible={this.state.verify}
+              animation="fade up"
+              duration={1000}
+            >
+              <Form onSubmit={this.register}>
+                <Header as="h1" icon className="prompt">
+                  <Icon name="pencil alternate" />
+                  ลงทะเบียนผู้ใช้ใหม่
+                  <Header.Subheader className="subheader">
+                    {this.state.loading
+                      ? "กรุณารอสักครู่ กำลังประมวลผลค่ะ"
+                      : "โปรดกรอกข้อมูลก่อนเริ่มใช้งานด้วยค่ะ"}
+                  </Header.Subheader>
+                </Header>
+                <Form.Field required>
+                  <label className="form-label">ชื่อเกษตรกร</label>
+                  <input
+                    required
+                    className="form-input"
+                    placeholder="กรอกชื่อที่นี่"
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    disabled={this.state.loading}
+                    onChange={this.changeHandler}
+                  />
+                </Form.Field>
+                <Form.Field required>
+                  <label className="form-label">เบอร์โทรศัพท์</label>
+                  <input
+                    required
+                    className="form-input"
+                    placeholder="กรอกเบอร์โทรศัพท์ที่นี่"
+                    maxLength="10"
+                    type="tel"
+                    name="tel"
+                    value={this.state.tel}
+                    disabled={this.state.loading}
+                    onChange={this.changeHandler}
+                  />
+                </Form.Field>
+                <Button
+                  color="orange"
+                  content="ลงทะเบียน"
+                  className="form-btn"
+                  type="submit"
                   disabled={this.state.loading}
-                  onChange={this.changeHandler}
+                  loading={this.state.loading}
                 />
-              </Form.Field>
-              <Form.Field required>
-                <label className="form-label">เบอร์โทรศัพท์</label>
-                <input
-                  required
-                  className="form-input"
-                  placeholder="กรอกเบอร์โทรศัพท์ที่นี่"
-                  maxLength="10"
-                  type="tel"
-                  name="tel"
-                  value={this.state.tel}
-                  disabled={this.state.loading}
-                  onChange={this.changeHandler}
-                />
-              </Form.Field>
-              <Button
-                color="orange"
-                content="ลงทะเบียน"
-                className="form-btn"
-                type="submit"
-                disabled={this.state.loading}
-                loading={this.state.loading}
-              />
-            </Form>
-          </Transition>
-        </div>
+              </Form>
+            </Transition>
+          </div>
+        ) : (
+          // Loader when entering register pages.
+          <div id="loader" className="main">
+            <Transition
+              visible={this.state.loader}
+              animation="fade up"
+              duration={1000}
+            >
+              <div>
+                <Loader size="massive" active indeterminate inverted>
+                  <Header as="h2" className="prompt">
+                    <br/>
+                    กำลังโหลดข้อมูล...
+                    <Header.Subheader className="subheader">
+                      กรุณารอสักครู่ค่ะ
+                      <span role="img" aria-label="please">
+                        {" "}
+                        🥺
+                      </span>
+                    </Header.Subheader>
+                  </Header>
+                </Loader>
+              </div>
+            </Transition>
+          </div>
+        )}
       </div>
     );
   }
